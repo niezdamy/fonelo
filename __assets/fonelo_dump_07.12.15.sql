f4b96e8cd7a1036fa237f5add1bc5b5e82eee1a7
hera04 <przemek.hernik@hotmail.com>
2015-12-07 20:42:35 +0100

    Added __asset folder
    Tutaj wrzucajmy wszystkie przydatne pliki :)

---
+++ __assets/fonelo_dump_07.12.15.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Czas generowania: 07 Gru 2015, 20:39
+-- Wersja serwera: 5.5.42
+-- Wersja PHP: 5.6.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Baza danych: `fonelo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `trainings`
+--
+
+CREATE TABLE `trainings` (
+  `id_training` int(11) NOT NULL,
+  `user` varchar(20) NOT NULL,
+  `date` varchar(10) NOT NULL,
+  `duration` text NOT NULL,
+  `hr` int(11) NOT NULL,
+  `callories` int(11) NOT NULL,
+  `size` int(11) NOT NULL,
+  `weight` float NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `trainings`
+--
+ALTER TABLE `trainings`
+  ADD PRIMARY KEY (`id_training`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `trainings`
+--
+ALTER TABLE `trainings`
+  MODIFY `id_training` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
